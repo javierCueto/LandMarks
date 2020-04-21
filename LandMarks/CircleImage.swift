890842dfbdd68541b48d3c4d1e1ae93a549e098b
Javier <javelie@outlook.com>
2020-04-20 20:30:04 -0600

    views and map

---
+++ LandMarks/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CilcleImage.swift
+//  LandMarks
+//
+//  Created by José Javier Cueto Mejía on 20/04/20.
+//  Copyright © 2020 Pozolx. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+           Image("coldplay")
+                 .resizable()
+                 .aspectRatio(contentMode: .fit)
+                 .clipShape(Circle())
+                 .overlay(
+                     Circle().stroke(Color.white, lineWidth: 4))
+                 .shadow(radius: 10)
+    }
+}
+
+struct CilcleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
